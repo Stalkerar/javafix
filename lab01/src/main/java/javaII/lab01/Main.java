50573e8dd11819404bab78890d5ee15b2049f528
Stalkerar <61618875+Stalkerar@users.noreply.github.com>
2021-02-19 11:53:20 +0100

    Add files via upload

---
+++ lab01/src/main/java/javaII/lab01/Main.java
@@ -0,0 +1,13 @@
+package javaII.lab01;
+
+//import javaII.birthdaylib.BirthdayEvaluator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//System.out.println(new BirthdayEvaluator(15, 5, 2000).age());
+		//System.out.println(new Calculator(1,2).solver());
+		App.main(args);
+	}
+
+}
---
+++ lab01/src/main/java/javaII/lab01/Pet.java
@@ -0,0 +1,9 @@
+package javaII.lab01;
+
+public class Pet {
+
+	public int count(int a, int b) {
+		return a*b;
+	}
+	
+}
